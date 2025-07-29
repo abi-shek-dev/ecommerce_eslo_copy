a82ed0de8caa77c415cb0534f0346a6b36c50b8b
Abishek-Web-Co <abishekinterspace@gmail.com>
2025-07-29 22:46:04 +0530

    Collections

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
+    <div className='mt-20'>
       <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 text-sm'>
 
         <div>
