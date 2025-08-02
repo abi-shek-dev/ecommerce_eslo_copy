ce7ee39bbbb47590369c0519462a0d747e4a61a6
Abishek-Web-Co <abishekinterspace@gmail.com>
2025-08-02 15:14:44 +0530

    About UI

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import { assets } from '../assets/assets'
+import NewsLetterBox from '../components/NewsLetterBox'
 
 const About = () => {
   return (
@@ -19,6 +20,28 @@ const About = () => {
           <p>Our mission at Forever is to empower customers with choice, convenience, and confidence. We're dedicated to providing a seamless shopping experience that exceeds expectations, from browsing and ordering to delivery and beyond.</p>
         </div>
       </div>
+
+      <div className='text-xl py-4'>
+        <Title text1={'WHY'} text2={'CHOOSE US'} />
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
+        </div>
+        <div className='border px-10 md:px-16 sm:py-20 flex flex-col gap-5'>
+          <b>Convenience:</b>
+          <p className='text-gray-600'>With our user-friendly interface and hassle-free ordering process, shopping has never been easier.</p>
+        </div>
+        <div className='border px-10 md:px-16 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer Service:</b>
+          <p className='text-gray-600'>Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is our top priority.</p>
+        </div>
+      </div>
+
+      <NewsLetterBox />
+
 
     </div>
   )
