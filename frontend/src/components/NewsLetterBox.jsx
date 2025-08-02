d6003da508d966b8d8d40f53077a80fc97e8246c
Abishek-Web-Co <abishekinterspace@gmail.com>
2025-08-02 15:40:43 +0530

    Contact Us & News Letter Box

--- frontend/src/components/NewsLetterBox.jsx
+++ frontend/src/components/NewsLetterBox.jsx
@@ -14,7 +14,7 @@ const NewsLetterBox = () => {
         </p>
         <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap- 3 mx-auto my-6 border pl-3'>
             <input className='w-full sm:flex-1 outline-none' required type="email" placeholder='Enter your email' />
-            <button type='submit' className='bg-black text-white text-xs px-10 py-4'>Subscribe</button>
+            <button type='submit' className='bg-black text-white text-xs px-10 py-4 cursor-pointer'>Subscribe</button>
         </form>
     </div>
   )
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Title from '../components/Title'
 import { assets } from '../assets/assets'
+import NewsLetterBox from '../components/NewsLetterBox'
 
 const Contact = () => {
   return (
@@ -21,6 +22,8 @@ const Contact = () => {
           <button className='border border-black px-8 py-4 hover:bg-black hover:text-white transition-all duration-500 cursor-pointer'>Explore Jobs</button>
         </div>
       </div>
+
+      <NewsLetterBox />
 
     </div>
   )
