bb1b4a38a3d0c13268b1b1a77d447e2cffae05a6
Abishek-Web-Co <abishekinterspace@gmail.com>
2025-08-03 13:23:08 +0530

    User Controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,18 @@
+
+
+// Route for user login
+const loginUser = async (req, res) => {
+
+}
+
+// Route for user signup
+const registerUser = async (req, res) => {
+
+}
+
+// Route for Admin Login
+const adminLogin = async (req, res) => {
+
+}
+
+export {loginUser, registerUser, adminLogin};
