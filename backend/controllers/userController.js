d0e41a21894fe387d836a1b5127cd896922ee735
Abishek-Web-Co <abishekinterspace@gmail.com>
2025-08-03 13:48:46 +0530

    Hashing password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,6 @@
+import validator from 'validator';
+import userModel from "../models/userModel.js";
+import bcrypt from 'bcrypt';
 
 
 // Route for user login
@@ -7,9 +10,31 @@ const loginUser = async (req, res) => {
 
 // Route for user signup
 const registerUser = async (req, res) => {
+    try {
 
-    res.json({message: "Register User Working"});
+        const { name, email, password } = req.body;
 
+        //  Check if the user already exists in the database
+        const exists = await userModel.findOne({ email });
+        if (exists) {
+            return res.json({ success: false, message: "User Already Exists" })
+        }
+
+        //  validating emsil and strong password
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "Please enter a valid email" })
+        }
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Please enter a strong password" })
+        }
+
+        // Hashing Password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+    } catch (error) {
+
+    }
 }
 
 // Route for Admin Login
@@ -17,4 +42,4 @@ const adminLogin = async (req, res) => {
 
 }
 
-export {loginUser, registerUser, adminLogin};
+export { loginUser, registerUser, adminLogin };
