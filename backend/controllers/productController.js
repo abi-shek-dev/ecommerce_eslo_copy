d17981c2cfdb3d1f0090957da964a2e74bf6fda5
Abishek-Web-Co <abishekinterspace@gmail.com>
2025-08-03 21:50:13 +0530

    Product Controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,23 +3,40 @@
 //  function for add product 
 const addProduct = async (req, res) => {
 
-    
+    try {
+
+        const { name, description, price, category, subCategory, sizes, bestSeller } = req.body;
+
+        const image1 = req.files.image1[0];
+        const image2 = req.files.image2[0];
+        const image3 = req.files.image3[0];
+        const image4 = req.files.image4[0];
+
+        console.log(name, description, price, category, subCategory, sizes, bestSeller);
+        console.log(image1, image2, image3, image4);
+
+        res.json({ message: "product added successfully" });
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 
 }
 
 //  function for list product 
 const listProduct = async (req, res) => {
-    
+
 }
 
 //  function for removing product 
 const removeProduct = async (req, res) => {
-    
+
 }
 
 //  function for single product info
 const singleProduct = async (req, res) => {
-    
+
 }
 
-export {listProduct,addProduct,removeProduct,singleProduct}
+export { listProduct, addProduct, removeProduct, singleProduct }
