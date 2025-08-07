97d53cc3479de14ff0d5ea5b825488626ad0b95f
Abishek-Web-Co <abishekinterspace@gmail.com>
2025-08-07 23:06:25 +0530

    Best Seller Error Fix

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,7 +6,7 @@ const addProduct = async (req, res) => {
 
     try {
 
-        const { name, description, price, category, subCategory, sizes, bestSeller } = req.body;
+        const { name, description, price, category, subCategory, sizes, bestseller } = req.body;
 
         const image1 = req.files.image1 && req.files.image1[0];
         const image2 = req.files.image2 && req.files.image2[0];
@@ -28,7 +28,7 @@ const addProduct = async (req, res) => {
             category,
             price: Number(price),
             subCategory,
-            bestSeller: bestSeller === 'true' ? true : false,
+            bestseller: bestseller === 'true' || bestseller === true,
             sizes: JSON.parse(sizes),
             image: imagesUrl,
             date: Date.now()
