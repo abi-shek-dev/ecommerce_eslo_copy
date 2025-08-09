edf9954e73daa2a75d8173513856de0fcd20d7c2
Abishek-Web-Co <abishekinterspace@gmail.com>
2025-08-09 22:09:48 +0530

    Order Model Controller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,39 @@
+
+
+// Placing order using cod method 
+
+const placeOrder = async (req, res) => {
+
+}
+
+//  placing order using stripe method 
+
+const placeOrderStripe = async (req, res) => {
+
+}
+
+//  placing order using Razorpay method 
+
+const placeOrderRazorPay = async (req, res) => {
+
+}
+
+// All Orders data for Admin Panel
+
+const allOrders = async (req, res) => {
+
+}
+
+// User Order Data for frontend
+
+const userOrders = async (req, res) => {
+
+}
+
+//  update status of the order from Admin panel
+
+const updateStatus = async (req, res) => {
+
+}
+
+export {placeOrder, placeOrderStripe, placeOrderRazorPay, allOrders, userOrders, updateStatus}
