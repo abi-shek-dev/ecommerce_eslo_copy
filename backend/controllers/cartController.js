b5b97a602b295479a303d27a43cf281380b8d9a3
Abishek-Web-Co <abishekinterspace@gmail.com>
2025-08-09 07:11:50 +0530

    Cart Router

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,21 @@
+
+//  Add Products to user cart
+
+const addToCart = async (req, res) => {
+    
+}
+
+
+//  update user cart
+
+const updateCart = async (req, res) => {
+    
+}
+
+
+//  get user cart data
+const getUserCart = async (req, res) => {
+    
+}
+
+export {addToCart, updateCart, getUserCart}
