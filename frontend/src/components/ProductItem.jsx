91900f89b607bb90622ccfe37f6d798d113512d1
Abishek-Web-Co <abishekinterspace@gmail.com>
2025-08-12 11:18:13 +0530

    Commit

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -8,8 +8,8 @@ const ProductItem = ({id,image,name,price}) => {
 
   return (
     <Link to={`/product/${id}`} className='text-gray-700 cursor-pointer'>
-        <div className='overflow-hidden'>
-            <img className='hover:scale-110 transition ease-in-out rounded-xl hover:rounded-xl ' src={image[0]} alt="" />
+        <div className='overflow-hidden rounded-xl'>
+            <img className='hover:scale-110 transition ease-in-out rounded-xl ' src={image[0]} alt="" />
         </div>
         <p className='pt-3 pb-1 text-sm'>{name}</p>
         <p className='text-sm font-medium'>{currency}{price}</p>
